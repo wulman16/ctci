442cb44dde52de9c97761983f0f84f7cb5e9ee42
wulman16 <will.ulman@gmail.com>
2019-06-29 11:38:53 -0400

    Add solution for string compression

--- string-compression.js
+++ string-compression.js
@@ -3,14 +3,24 @@
  *
  * Compress a given string using the counts of repeated characters
  *
- * Time complexity:
- * Space complexity:
+ * Time complexity: O(n)
+ * Space complexity: O(n)
  *
  * @param {string} str
  * @return {boolean}
  */
 const compressString = str => {
-  return str;
+  let output = ``;
+  let currentRun = 1;
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i - 1]) currentRun++;
+    else {
+      output += str[i - 1] + currentRun.toString();
+      currentRun = 1;
+    }
+  }
+  output += str[str.length - 1] + currentRun.toString();
+  return str.length <= output.length ? str : output;
 };
 
 module.exports = compressString;
