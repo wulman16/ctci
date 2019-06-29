2f97520279293cfa08768138d92199d7b625ab0a
wulman16 <will.ulman@gmail.com>
2019-06-29 11:53:42 -0400

    Add tests for rotate matrix problem

---
+++ arrays-and-strings/rotate-matrix.js
@@ -0,0 +1,16 @@
+/**
+ * Rotate Matrix
+ *
+ * Rotate an NxN matrix by 90 degrees
+ *
+ * Time complexity:
+ * Space complexity:
+ *
+ * @param {Array<Array<number>>} matrix
+ * @return {Array<Array<number>>}
+ */
+const rotateMatrix = matrix => {
+  return matrix;
+};
+
+module.exports = rotateMatrix;
---
+++ arrays-and-strings/rotate-matrix.test.js
@@ -0,0 +1,19 @@
+const rotateMatrix = require(`./rotate-matrix`);
+
+test(`works in basic cases`, () => {
+  expect(rotateMatrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).toStrictEqual([
+    [7, 4, 1],
+    [8, 5, 2],
+    [9, 6, 3]
+  ]);
+  expect(rotateMatrix([[1, 2], [4, 5]])).toStrictEqual([[4, 1], [5, 2]]);
+  expect(rotateMatrix([[1]])).toStrictEqual([[1]]);
+});
+
+test(`works on N=1 matrices`, () => {
+  expect(rotateMatrix([[1]])).toStrictEqual([[1]]);
+});
+
+test(`works on empty matrices`, () => {
+  expect(rotateMatrix([[]])).toStrictEqual([[]]);
+});
