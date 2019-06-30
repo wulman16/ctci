4761077c2b997ec0c42bc3471d57719bd9ff5420
wulman16 <will.ulman@gmail.com>
2019-06-30 13:14:38 -0400

    Add initial solution and tests for remove duplicates problem

--- linked-lists/remove-duplicates.js
+++ linked-lists/remove-duplicates.js
@@ -10,6 +10,21 @@
  * @return {SinglyLinkedList}
  */
 const removeDuplicates = sll => {
+  if (sll.length <= 1) return sll;
+  const seenValues = {};
+  let currentNode = sll.head;
+  let i = 0;
+  let length = sll.length;
+  while (i < length) {
+    if (seenValues[currentNode.val]) {
+      sll.remove(i);
+      length--;
+    } else {
+      seenValues[currentNode.val] = true;
+      i++;
+    }
+    currentNode = currentNode.next;
+  }
   return sll;
 };
 
--- linked-lists/remove-duplicates.test.js
+++ linked-lists/remove-duplicates.test.js
@@ -1,2 +1,51 @@
 const removeDuplicates = require(`./remove-duplicates`);
 const SLL = require(`./SinglyLinkedList`);
+
+const sll1 = new SLL();
+sll1.push(1);
+sll1.push(2);
+sll1.push(1);
+sll1.push(1);
+sll1.push(2);
+sll1.push(3);
+
+const sll1Output = new SLL();
+sll1Output.push(1);
+sll1Output.push(2);
+sll1Output.push(3);
+
+const sll2 = new SLL();
+sll2.push(5);
+sll2.push(10);
+sll2.push(4);
+sll2.push(3);
+
+const sll3 = new SLL();
+sll3.push(1);
+
+const sll4 = new SLL();
+sll4.push(1);
+sll4.push(1);
+sll4.push(1);
+
+sll5 = new SLL();
+
+test(`works in basic cases`, () => {
+  expect(removeDuplicates(sll1)).toStrictEqual(sll1Output);
+});
+
+test(`removes no nodes when there are no duplicates`, () => {
+  expect(removeDuplicates(sll2)).toStrictEqual(sll2);
+});
+
+test(`removes no nodes from a singleton SLL`, () => {
+  expect(removeDuplicates(sll3)).toStrictEqual(sll3);
+});
+
+test(`works on empty SLLs`, () => {
+  expect(removeDuplicates(sll5)).toStrictEqual(sll5);
+});
+
+test(`removes all duplicates when all nodes are identical`, () => {
+  expect(removeDuplicates(sll4)).toStrictEqual(sll3);
+});
